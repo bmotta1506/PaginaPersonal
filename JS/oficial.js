a0a66a0075c4ca1f6b892aa3cf0eae16b433e793
bmotta1506 <bmotta1506@gmail.com>
2025-12-20 15:25:29 -0500

    footer y header terminados con rsponsive

--- JS/oficial.js
+++ JS/oficial.js
@@ -1,9 +1,15 @@
 const hamburger = document.getElementById('hamburger');
 const navMenu = document.getElementById('navOficial');
 
+const searchTrigger = document.getElementById('search-trigger');
+const mobileSearch = document.getElementById('mobile-search-bar');
+const closeSearch = document.getElementById('close-search');
+
 hamburger.addEventListener('click', () => {
     navMenu.classList.toggle('active');
-     hamburger.classList.toggle('open');
+    hamburger.classList.toggle('open');
+
+    if (mobileSearch) mobileSearch.classList.remove('active');
 });
 
 document.querySelectorAll('#navOficial ul li a').forEach(link => {
@@ -12,3 +18,18 @@ document.querySelectorAll('#navOficial ul li a').forEach(link => {
         hamburger.classList.remove('open');
     });
 });
+
+if (searchTrigger) {
+    searchTrigger.addEventListener('click', () => {
+        mobileSearch.classList.add('active');
+        
+        navMenu.classList.remove('active');
+        hamburger.classList.remove('open');
+    });
+}
+
+if (closeSearch) {
+    closeSearch.addEventListener('click', () => {
+        mobileSearch.classList.remove('active');
+    });
+}
